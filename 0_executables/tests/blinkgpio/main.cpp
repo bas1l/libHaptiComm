5113de7087dae90b98a1a175449828faaf70d1ec
bas1l <basil.duvernoy@gmail.com>
2018-11-03 18:28:30 +0100

    release mistake

--- 0_executables/tests/blinkgpio/main.cpp
+++ 0_executables/tests/blinkgpio/main.cpp
@@ -4,20 +4,24 @@ int main (void)
   
     //int numGPIO=4;
   wiringPiSetup () ;
-  pinMode (numGPIO, OUTPUT) ;
-  while(true){
+  
     for (int numGPIO=0;numGPIO<28;numGPIO++)
     {
-      digitalWrite (numGPIO, HIGH) ;
+        pinMode (numGPIO, OUTPUT) ;
     }
-    delay (500) ;
+    while(true){
+        for (int numGPIO=0;numGPIO<28;numGPIO++)
+        {
+            digitalWrite (numGPIO, HIGH) ;
+        }
+        delay (500) ;
 
-    for (int numGPIO=0;numGPIO<28;numGPIO++)
-    {
-      digitalWrite (numGPIO,  LOW) ;
-    } 
-    delay (500) ;
-  }
+        for (int numGPIO=0;numGPIO<28;numGPIO++)
+        { 
+            digitalWrite (numGPIO,  LOW) ;
+        } 
+        delay (500) ;
+    }
   
   
   return 0 ;
