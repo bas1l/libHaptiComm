96487163394e7db0df71eff7e7ce9b350bea1554
duvernoy <basil.duvernoy@gmail.com>
2019-07-10 03:35:41 +0100

    commit braille japanese and english v2.0

--- 0_executables/haptiBraille/haptiBraille.cpp
+++ 0_executables/haptiBraille/haptiBraille.cpp
@@ -33,15 +33,34 @@
 #define PROSODY_SENTENCE 3
 
 // all in milliseconds
-#define PROSODY_LETTER_DELAY 	100
-#define PROSODY_WORD_DELAY   	200
+#define PROSODY_LETTER_DELAY 	700
+#define PROSODY_WORD_DELAY   	1000
 #define PROSODY_SENTENCE_DELAY  500
-#define DURATION_SYMBOL  	50
+#define DURATION_SYMBOL  	700
+
+// keyboard keys
+#define KEY_ESC 27 // escape key
+#define KEY_ERASE 263 // erase key
+#define KEY_CTRLC 0x03 // CTRL+C
+#define KEY_Fmin 265
+#define KEY_Fmax 276
+#define KEY_F1 265
+#define KEY_F2 266
+#define KEY_F3 267
+#define KEY_F4 268
+#define KEY_F5 269
+#define KEY_F6 270
+#define KEY_F7 271
+#define KEY_F8 272
+#define F_KEYPRESS(k) (k>=KEY_Fmin && k<=KEY_Fmax)
+#define EXIT_KEYPRESS(k) (k==KEY_ESC || k==KEY_CTRLC)
+
+int STATE_PROGRAM;
 
 std::mutex m_mutex;
 std::condition_variable m_condVar;
 std::atomic<bool> workdone;
-std::queue<char> sentences;
+std::deque<char> sentences;
 bool sentencesEmpty;
 
 void generateSentences(std::atomic<bool> & workdone, std::string str_alph);
@@ -53,8 +72,9 @@ int  setOpt(int *argc, char *argv[], int * prosody, const char *& cfgSource, con
 static void usage();
 
 bool signal4keypressed();
-void copySentences(std::deque<char> * letters);
+void addSentencesInto(std::deque<char> * letters);
 void addto_sentence(char ch);
+bool rmto_sentence();
 bool is_sentencesEmpty();
 void jobdone();
 bool is_jobdone();
@@ -65,89 +85,111 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /* CREATE VARIABLES */
-    const char * cfgSource;
-    const char * scope;
-    int exitStatus = 0, prosody;
-    std::thread extract_text;
-    std::thread send_to_dac;
+  /* SETUP ENVIRONEMENT */
+  /*  printw and timer  */
+  std::cout<<"[initialisation] ...done."<<std::endl;
+  struct timespec t;
+  struct sched_param param;
+  param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+  if(sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
+      perror("sched_setscheduler failed");
+      exit(-1);
+  }
+  if(mlockall(MCL_CURRENT|MCL_FUTURE) == -1) {
+      perror("mlockall failed");
+      exit(-2);
+  }
+  setlocale(LC_ALL, "");
+  initscr();
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+    
+  /* CREATE VARIABLES */
+  const char * cfgSource;
+  const char * scope;
+  int exitStatus = 0, prosody;
+  std::thread extract_text;
+  std::thread send_to_dac;
+
+  // time of prosody (letter, word or sentence rythms)  
+  switch (prosody)
+  {
+    case PROSODY_WITHOUT:
+        std::cout<<"PROSODY_WITHOUT"<<std::endl;
+        break;
+    case PROSODY_LETTER:
+        std::cout<<"PROSODY_LETTER"<<std::endl;
+        break;
+    case PROSODY_WORD:
+        std::cout<<"PROSODY_WORD"<<std::endl;
+        break;
+    case PROSODY_SENTENCE:
+        std::cout<<"PROSODY_SENTENCE"<<std::endl;
+        break;
+  }
+
+  STATE_PROGRAM = 0;
+  do {
+    
+    // init empty screen
+    erase();
+    
+    /* INITIALISE VARIABLES */
     HaptiCommConfiguration * cfg = new HaptiCommConfiguration();
     DEVICE 	 * dev  = new DEVICE();
     WAVEFORM * wf   = new WAVEFORM();
     ALPHABET * alph = new ALPHABET();
 
-    /*INITIALISE GLOBAL VARIABLES
-     * 
-     */
+    /* INITIALISE GLOBAL VARIABLES */
     workdone = false;
     sentencesEmpty = true;
-    /* INITIALISE VARIABLES
-     * 
-     */
-    /* time of prosody (letter, word or sentence rythms) */
-    setOpt(&argc, argv, &prosody, cfgSource, scope);
-    cfg->configure(cfgSource, dev, wf, alph);
-    std::cout<<"[initialisation][options] prosody=";
-    switch (prosody)
-    {
-      case PROSODY_WITHOUT:
-          std::cout<<"PROSODY_WITHOUT"<<std::endl;
-          break;
-      case PROSODY_LETTER:
-          std::cout<<"PROSODY_LETTER"<<std::endl;
-          break;
-      case PROSODY_WORD:
-          std::cout<<"PROSODY_WORD"<<std::endl;
-          break;
-      case PROSODY_SENTENCE:
-          std::cout<<"PROSODY_SENTENCE"<<std::endl;
-          break;
-    }
-    std::cout<<"[initialisation][options] cfgSource="<<cfgSource<<std::endl;
-    std::cout<<"[initialisation] ...done."<<std::endl;
-    /* SETUP ENVIRONEMENT
-     * printw and timer
-     *
-    */
-    struct timespec t;
-    struct sched_param param;
-    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    if(sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
-		    perror("sched_setscheduler failed");
-		    exit(-1);
-    }
-    if(mlockall(MCL_CURRENT|MCL_FUTURE) == -1) {
-		    perror("mlockall failed");
-		    exit(-2);
-    }
-    setlocale(LC_ALL, "");
-
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    noecho();
     
-    /* INITIALISE THREAD
-     * 
-     */
+    switch (STATE_PROGRAM) {
+      case 0:
+        // initialise the first config file
+        setOpt(&argc, argv, &prosody, cfgSource, scope);
+        std::cout<<"[initialisation][options] cfgSource="<<cfgSource<<std::endl;
+        break;
+      case KEY_F1: // Japanese Braille
+        prosody = PROSODY_WORD;
+        cfgSource = "libHaptiComm/4_configuration/config_JapaneseBraille.cfg";
+        break;
+      case KEY_F5: // English Braille
+        prosody = PROSODY_LETTER;
+        cfgSource = "libHaptiComm/4_configuration/configHaptiBraille_newDriver.cfg";
+        break;
+      case KEY_F6: // English Braille
+        prosody = PROSODY_WORD;
+        cfgSource = "libHaptiComm/4_configuration/configHaptiBraille_newDriver.cfg";
+        break;
+      case KEY_F7: // English Braille
+        prosody = PROSODY_SENTENCE;
+        cfgSource = "libHaptiComm/4_configuration/configHaptiBraille_newDriver.cfg";
+        break;
+      default: 
+        break;
+    }
+
+    /* INITIALISE */
+    cfg->configure(cfgSource, dev, wf, alph);
     extract_text = std::thread(workSymbols, std::ref(workdone), std::ref(alph), std::ref(dev), prosody);
     send_to_dac = std::thread( generateSentences, std::ref(workdone), alph->getlistSymbols());
     
-    /* WORK
-     * 
-     */
+    /* WORK */
     extract_text.join();
     send_to_dac.join();
-    
-    /* CLEAN
-     * 
-     */
+      
+    /* CLEAN */
     delete cfg;
     delete dev;
     delete wf;
     delete alph;
-    
-    return exitStatus;
+
+  } while (!EXIT_KEYPRESS(STATE_PROGRAM));
+
+  
+  return exitStatus;
 }
 
 
@@ -156,41 +198,53 @@ void generateSentences(std::atomic<bool> & workdone, std::string str_alph)
   std::string s = "the quick brown fox jumps over the lazy dog";
   std::string str_prosody = " .";
   std::string str_ponc = "',;:!?-";
-  //printw("alphabet:%s", str_alph.c_str());
-  
+  bool stop_read = false, lastis_pod; // part of dictionnary
   int ch;
   
   printw("You can start to write a letter, a word, a sentence \n");
-  printw(" --- When you are done, press '*' to Exit ---\n");
-  do{
-    if (ch != ERR)
+  printw(" --- When you are done, press <ESCAPE> or <CTRL+C> to Exit ---\n");
+  printw("<F1> = Japanese Braille | <F5> = English Braille (letter rythm) | <F1> = English Braille (word rythm) | <F1> = English Braille (sentence rythm)\n");
+  lastis_pod = false;
+  do {
+    ch = getch();
+    /*
+    for (int x=0x000; x<=0xfff; x++)
     {
-      // if part of the alphabet
-      if (  str_alph.find(ch) != std::string::npos ||
-            str_prosody.find(ch) != std::string::npos)
+      if (ch == x)
       {
-        printw("%c", ch);
-        addto_sentence(ch);
-      }// if part of the ponctuation
-      else if (str_ponc.find(ch) != std::string::npos)
-      {
-        printw("%c", ch);
-      }
-      else if (ch == '0')
-      {
-        // printw("%s. ", s);
-        for(int i=0; i<s.length();++i)
-        {
-          printw("%c", s[i]);
-          addto_sentence(s[i]);
-        }
-      }
-      else
-      {
-        //printw("\n<Key not implemented> Need to Exit ? Press '*'.\n");
+        printw("<%i>", x);
       }
     }
-  }while((ch = tolower(getch())) != '*');
+    */
+    if (EXIT_KEYPRESS(ch) || F_KEYPRESS(ch)) { // escape or CTRL+C or function keys
+      STATE_PROGRAM = ch;
+      stop_read = true;
+    }
+//    else if (ch == KEY_ERASE)
+//    {
+//      rmto_sentence();
+//      delch();
+//      refresh();
+//    }
+    else if (str_alph.find(tolower(ch)) != std::string::npos ||
+             str_prosody.find(ch) != std::string::npos) {
+      printw("%c", ch);
+      addto_sentence(ch);
+    }
+    else if (str_ponc.find(ch) != std::string::npos) {
+      printw("%c", ch);
+    }
+    else if (ch == '0') {
+      for(int i=0; i<s.length();++i) {
+        printw("%c", s[i]);
+        addto_sentence(s[i]);
+      }
+    }
+    else {
+      // <Key not implemented> 
+      // printw("?");
+    }
+  } while(!stop_read);
   
   
   printw("\tWHC::create_sentences::End\n");
@@ -198,6 +252,7 @@ void generateSentences(std::atomic<bool> & workdone, std::string str_alph)
   endwin();
   
   jobdone();
+  
 }
 
 
@@ -224,7 +279,7 @@ void workSymbols(std::atomic<bool> & workdone, ALPHABET *& alph, DEVICE * dev, i
   while(!is_jobdone())
   {
     signal4keypressed();
-    copySentences(&letters);
+    addSentencesInto(&letters);
     
     // if last char is a space, then a word is finished
     if ((letters.front() == '+') || (letters.front() == '-'))
@@ -277,7 +332,7 @@ void workSymbols(std::atomic<bool> & workdone, ALPHABET *& alph, DEVICE * dev, i
             
             if (found)
             {
-              hb_driver.executeSymbol6by6(v, duration_ms, PROSODY_LETTER_DELAY/1.5);
+              hb_driver.executeSymbol6by6(v, duration_ms, PROSODY_WORD_DELAY);
               v.clear();
               std::this_thread::sleep_for(std::chrono::milliseconds(PROSODY_LETTER_DELAY));
             }
@@ -487,17 +542,40 @@ int setOpt(int *argc, char *argv[], int * prosody, const char *& cfgSource, cons
 void addto_sentence(char ch)
 {
 	std::lock_guard<std::mutex> lk(m_mutex); // locker to access shared variables
-	sentences.push(ch);
+	sentences.push_back(ch);
 	sentencesEmpty = false;
 	m_condVar.notify_one(); // waiting thread is notified 
 }
 
-void copySentences(std::deque<char> * letters)
+
+bool rmto_sentence()
+{
+  bool success = true;
+  
+	std::lock_guard<std::mutex> lk(m_mutex); // locker to access shared variables
+	if (is_sentencesEmpty()) {
+    success = false;
+  }
+  else if (sentences.size() == 1) {
+    sentences.pop_back();
+    sentencesEmpty = true;
+    success = true;
+  }
+  else {
+    sentences.pop_back();
+    success = true;
+  }
+	m_condVar.notify_one(); // waiting thread is notified 
+  
+  return success;
+}
+
+void addSentencesInto(std::deque<char> * letters)
 {
 	std::unique_lock<std::mutex> mlock(m_mutex);
 	while (!sentences.empty()){
 		letters->push_back(sentences.front());
-		sentences.pop();
+		sentences.pop_front();
 	}
 	sentencesEmpty = true;
 	m_condVar.notify_one(); // waiting thread is notified 
