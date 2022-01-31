c9c5bd6277d62c26306dd3451538eae903900864
Basil Duvernoy <basil.duvernoy@gmail.com>
2022-01-31 08:53:24 +0100

    modifications standard hapticomm file

--- 5_runScripts/haptiComm.sh
+++ 5_runScripts/haptiComm.sh
@@ -1,1 +1,1 @@
-sudo ./build/0_executables/demo_standard/demo --cfg libHaptiComm/4_configuration/configBlack.cfg $@
+sudo ./build/0_executables/demo_standard/demo --cfg libHaptiComm/4_configuration/configBlue.cfg $@
